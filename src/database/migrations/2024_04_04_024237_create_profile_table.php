774981109b5c9a908933a680c09935d186fad2e7
Hakdoff <erikafuerte09@gmail.com>
2024-04-04 10:49:29 +0800

    Modified Profiles table

--- src/database/migrations/2024_04_04_024237_create_profile_table.php
+++ src/database/migrations/2024_04_04_024237_create_profile_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('profile', function (Blueprint $table) {
+        Schema::create('profiles', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained();
             $table->string('first_name', '255');
