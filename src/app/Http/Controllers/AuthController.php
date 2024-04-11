1cd024efe30799f30906572e36547d27a709af4d
PeterMadrid <petermadrid0421@gmail.com>
2024-04-11 13:08:14 +0800

    email verif

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Models\Profile;
-// use App\Mail\RegisterMail;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Hash;
-// use Illuminate\Support\Facades\Mail;
 use App\Http\Requests\RegisterRequest;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Auth\Events\Verified;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -20,7 +22,7 @@ class AuthController extends Controller
                 'username' => $request->username,
                 'password' => Hash::make($request->password),
                 'email' => $request->email,
-                'status' => 0, 
+                'status' => 0,
                 'remember_token' => Str::random(40),
             ]);
 
@@ -30,21 +32,47 @@ class AuthController extends Controller
                 'last_name' => $request->last_name,
                 'middle_name' => $request->middle_name,
                 'birth_date' => $request->birthday,
-                'address' => $request->lot_block . ' ' . $request->street . ', ' .
-                    $request->city . ', ' . $request->province . ', ' .
-                    $request->country . ', ' . $request->zip_code,
+                'address' => implode(', ', array_filter([
+                    $request->lot_block,
+                    $request->street,
+                    $request->city,
+                    $request->province,
+                    $request->country,
+                    $request->zip_code,
+                ])),
             ]);
 
-            // $save = new User;
-            // $save->email = trim($request->email);
+            auth()->login($user);
+            event(new Registered($user));
 
-            // // event(new Registered($user));
-            // Mail::to($save->email)->send(new RegisterMail($save));
-
-            return redirect('/login')->with('success', 'User registered successfully!');
+            return redirect('/register')->with('success', 'Registration is successful! Please check your email for verification.');
         } catch (\Exception $e) {
-
             dd($e->getMessage());
         }
+    }
+
+    public function verifyEmail(Request $request, $id, $hash)
+    {
+        if (
+            $id == auth()->id() &&
+            auth()->user()->markEmailAsVerified()
+        ) {
+            event(new Verified($request->user()));
+        }
+
+        return redirect('/register')->with('verified', true);
+    }
+
+    public function verifyWaiting()
+    {
+
+        if (auth()->check()) {
+            if (auth()->check() && auth()->user()->email_verified_at) {
+                auth()->user()->status = 1;
+                auth()->user()->save();
+                return view('register');
+            }
+        }
+        return view('register');
     }
 }
