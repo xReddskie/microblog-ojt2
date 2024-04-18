c231ef8d5c983839a60c2298ab9cd8988ecd1c0c
Hakdoff <erikafuerte09@gmail.com>
2024-04-18 11:48:53 +0800

    Removed trail spacing

--- src/resources/views/pages/layouts/head.blade.php
+++ src/resources/views/pages/layouts/head.blade.php
@@ -2,4 +2,3 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/scss/app.scss'])
     <title>@yield('title')</title>
-    
