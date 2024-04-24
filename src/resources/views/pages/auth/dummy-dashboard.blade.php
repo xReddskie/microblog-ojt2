aed122ac91f97db263854e1fb30fd3f516e9776f
PeterMadrid <petermadrid0421@gmail.com>
2024-04-24 11:21:51 +0800

    Dashboard Front End initial commit

--- src/resources/views/pages/auth/dummy-dashboard.blade.php
+++ src/resources/views/pages/auth/dummy-dashboard.blade.php
@@ -5,11 +5,19 @@
 @section('content')
 
 <body>
-    @include('pages.dashboard.post')
-
-    <a href="/logout">
-        <button>logout</button>
-    </a>
+    <div class="d-dashboard">
+        @include('pages.dashboard.navbar')
+            <div class="d-dashboard__main">
+                <div class="d-dashboard__space"></div>
+                @include('pages.dashboard.sidebar')
+                <main class="d-dashboard__post-content">
+                    @include('pages.dashboard.post')
+                    @include('pages.dashboard.post-content')
+                </main>
+                @include('pages.dashboard.aside')
+            <div class="d-dashboard__space"></div>
+        </div>
+    </div>
 </body>
 
 </html>
