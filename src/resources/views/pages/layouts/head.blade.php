5e3491e157d79471ed94716e6f93b7b809efe6c9
PeterMadrid <petermadrid0421@gmail.com>
2024-04-25 17:23:32 +0800

    Moved js files to app.js. removed from vite

--- src/resources/views/pages/layouts/head.blade.php
+++ src/resources/views/pages/layouts/head.blade.php
@@ -1,4 +1,4 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/hidden-menu.js', 'resources/js/buttons.js', 'resources/scss/app.scss'])
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/scss/app.scss'])
     <title>@yield('title')</title>
