c8fbdde3ef7be71da7b7202ffaab01324d2c56d4
PeterMadrid <petermadrid0421@gmail.com>
2024-04-30 10:19:44 +0800

    Format

--- src/resources/js/post.js
+++ src/resources/js/post.js
@@ -1,10 +1,9 @@
 function openNav(postId) {
     document.getElementById("mySidenav" + postId).style.width = "100px";
     document.getElementById("filter" + postId).style.display = "block";
-  }
+}
 
-  function closeNav(postId) {
+function closeNav(postId) {
     document.getElementById("mySidenav" + postId).style.width = "0";
     document.getElementById("filter" + postId).style.display = "none";
-  }
-  
+}
