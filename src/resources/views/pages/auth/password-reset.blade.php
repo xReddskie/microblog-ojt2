b8bbd6dbf235682e0742e963260749dabfd1c42b
NamelessDweller <kaarushima755@gmail.com>
2024-05-02 15:03:37 +0800

    Added break line in password-reset.blade.php

--- src/resources/views/pages/auth/password-reset.blade.php
+++ src/resources/views/pages/auth/password-reset.blade.php
@@ -1,14 +1,14 @@
 @component('mail::message')
-# Reset Your Password
+    # Reset Your Password
 
-You are receiving this email because we received a password reset request for your account.
+    You are receiving this email because we received a password reset request for your account.
 
-@component('mail::button', ['url' => $url])
-Reset Password
-@endcomponent
+    @component('mail::button', ['url' => $url])
+        Reset Password
+    @endcomponent
 
-If you did not request a password reset, no further action is required.
+    If you did not request a password reset, no further action is required.
 
-Thanks,<br>
-{{ config('app.name') }}
+    Thanks,<br>
+    {{ config('app.name') }}
 @endcomponent
