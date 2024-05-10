16af1d4c4525941a012f50f67e12e13a8e83d406
Hakdoff <erikafuerte09@gmail.com>
2024-05-10 11:49:30 +0800

    UI for edit profile

--- src/resources/views/pages/profile/edit-profile.blade.php
+++ src/resources/views/pages/profile/edit-profile.blade.php
@@ -4,12 +4,11 @@
 
 @section('content')
 
-
-    <body>
-        <h1>Edit Your Profile</h1>
-
-        {{-- Success Message --}}
-        @if (session('status'))
+<body>
+    <div class="a-login bg-background">
+        <div class="p-profile__edit-container">
+            {{-- Success Message --}}
+            @if (session('status'))
             <div id="successMessage" style="color: green; background-color: #ccffcc; padding: 10px; margin-bottom: 15px;">
                 {{ session('status') }}
             </div>
@@ -18,11 +17,9 @@
                     window.location.href = "{{ route('profile-page') }}";
                 }, 5000);
             </script>
-        @endif
-
-
-        {{-- Error Messages --}}
-        @if ($errors->any())
+            @endif
+            {{-- Error Messages --}}
+            @if ($errors->any())
             <div style="color: red; background-color: #ffcccc; padding: 10px; margin-bottom: 15px;">
                 <ul>
                     @foreach ($errors->all() as $error)
@@ -31,36 +28,68 @@
                 </ul>
             </div>
         @endif
-
-        <form method="POST" action="{{ route('profile.update') }}">
-            @csrf
-            @method('PUT')
-
-            <label for="first_name">First Name:</label>
-            <input type="text" id="first_name" name="first_name" value="{{ $user->profile->first_name }}" required>
-
-            <label for="middle_name">Middle Name:</label>
-            <input type="text" id="middle_name" name="middle_name" value="{{ $user->profile->middle_name }}">
-
-            <label for="last_name">Last Name:</label>
-            <input type="text" id="last_name" name="last_name" value="{{ $user->profile->last_name }}" required>
-            
+    <div class="a-login__padding1"> </div>
+        <div class="a-login__h1 items-center">
+        <h1>Edit Your Profile</h1>
+    </div>
+    <div class="a-login__padding2"></div>
+    <hr>
+    <div class="a-login__padding1 w-96"></div>
+    <form method="POST" action="{{ route('profile.update') }}">
+        @csrf
+        @method('PUT')
+    <div class="items-start">
+        <div class="a-login__label">
+            <label for="first_name">Name:</label>
+        </div>
+        <div class="flex gap-2">
+            <input class="p-profile__edit-input" type="text" id="first_name" name="first_name" value="{{ $user->profile->first_name }}" required>
+            <input class="p-profile__edit-input" type="text" id="middle_name" name="middle_name" value="{{ $user->profile->middle_name }}">
+            <input class="p-profile__edit-input" type="text" id="last_name" name="last_name" value="{{ $user->profile->last_name }}" required>
+        </div>
+        <div class="a-login__label">
+            <div class="a-login__padding4 w-96">
+                <div class="w-full">
+                    <hr>
+                </div>
             <label for="bio">Bio:</label>
-            <input type="text" id="bio" name="bio" value="{{ $user->profile->bio }}" required>
-
+            </div>
+        </div>
+            <input class="p-profile__edit-input" type="text" id="bio" name="bio" value="{{ $user->profile->bio }}" required>
+        <div  class="a-login__padding4 w-full"><hr></div>
+        <div class="a-login__label">
             <label for="birth_date">Birth Date:</label>
-            <input type="date" id="birth_date" name="birth_date" value="{{ $user->profile->birth_date }}" required>
-
+        </div>
+            <input class="p-profile__edit-input" type="date" id="birth_date" name="birth_date" value="{{ $user->profile->birth_date }}" required>
+        <div class="a-login__padding4 w-full">
+            <hr>
+        </div>
+        <div class="a-login__label">
             <label for="address">Address:</label>
-            <input type="text" id="address" name="address" value="{{ $user->profile->address }}" required>
-
+        </div>
+            <input class="p-profile__edit-input-full" type="text" id="address" name="address" value="{{ $user->profile->address }}" required>
+        <div  class="a-login__padding4 w-full">
+            <hr>
+        </div>
+        <div class="a-login__label">
             <label for="phone_number">Phone Number:</label>
-            <input type="text" id="phone_number" name="phone_number" value="{{ $user->profile->phone_number }}" required>
-
+        </div>
+            <input class="p-profile__edit-input" type="text" id="phone_number" name="phone_number" value="{{ $user->profile->phone_number }}" required>
+        <div class="a-login__padding4 w-full">
+            <hr>
+        </div>
+        <div class="a-login__label">
             <label for="password">Confirm your password to save changes:</label>
-            <input type="password" id="password" name="password" required>
-
-            <button type="submit">Save Changes</button>
-        </form>
-    </body>
+        </div>
+            <input class="p-profile__edit-input" type="password" id="password" name="password" required>
+        </div>
+        <div class="a-login__padding4 w-full">
+            <hr>
+        </div>
+        <div class="p-profile__button">
+            <button class="a-login__button" type="submit">Save Changes</button>
+        </div>
+    </form>
+</div>
+</body>   
 @endsection
