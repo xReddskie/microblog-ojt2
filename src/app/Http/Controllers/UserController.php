bac982329c60a30d7ca17f20b295f570beb4cf50
PeterMadrid <petermadrid0421@gmail.com>
2024-05-14 17:55:16 +0800

    Added docbloc and return type

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use App\Models\User;
+use Illuminate\View\View;
 
 class UserController extends Controller
 {
@@ -11,7 +12,7 @@ class UserController extends Controller
      * Show other user's profile
      */
 
-    public function show($id)
+    public function show(int $id): View
     {
         $user = User::with('profile')->findOrFail($id);
         $posts = Post::where('user_id', $user->id)->get();
