74a8be588ef0299387bfd4f8bce35ad451b0ae80
Hakdoff <erikafuerte09@gmail.com>
2024-05-16 16:24:31 +0800

    added image upload in profile picture

--- src/database/migrations/2024_05_16_051227_add_profilepic_to_profiles_table.php
+++ src/database/migrations/2024_05_16_051227_add_profilepic_to_profiles_table.php
@@ -26,3 +26,31 @@ return new class extends Migration
         });
     }
 };
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('profiles', function (Blueprint $table) {
+            $table->string('images')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('profiles', function (Blueprint $table) {
+            //
+        });
+    }
+};
