4a403a388719a87f8fe70b51106bca2c3d5e1960
Hakdoff <erikafuerte09@gmail.com>
2024-05-21 09:19:39 +0800

    added dropcolumn

--- src/database/migrations/2024_05_16_051227_add_profilepic_to_profiles_table.php
+++ src/database/migrations/2024_05_16_051227_add_profilepic_to_profiles_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('profiles', function (Blueprint $table) {
-            //
+            $table->dropColumn('images');
         });
     }
 };
