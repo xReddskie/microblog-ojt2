45b329dfcf8c396a3da289b8e39ef511f4d0253c
Hakdoff <erikafuerte09@gmail.com>
2024-05-21 14:53:15 +0800

    Added profile and cover pic

--- src/app/Models/Profile.php
+++ src/app/Models/Profile.php
@@ -19,8 +19,6 @@ class Profile extends Model
         'address',
         'phone_number',
         'bio',
-        'profilepic',
-        'coverpic',
     ];
 
     /**
@@ -29,29 +27,5 @@ class Profile extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class, 'user_id');
-    }
-    
-    /**
-     * Get image URL
-     */
-    public function getImageURL(): string
-    {
-        if($this->profilepic){
-            $cleanedPath = str_replace('public/', '', $this->profilepic);
-            return url('storage/'. $cleanedPath);
-        }
-        return "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ{{$this->name}}";
-    }
-    
-    /**
-     * Get cover URL
-     */
-    public function getCoverURL(): string
-    {
-        if($this->coverpic){
-            $cleanedPath = str_replace('public/', '', $this->coverpic);
-            return url('storage/'. $cleanedPath);
-        }
-        return "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ{{$this->name}}";
     }
 }
