7f6f97a2615f80a0115ea7b2f0c1795c7c0d9cad
PeterMadrid <petermadrid0421@gmail.com>
2024-05-22 14:10:02 +0800

    UI

--- src/resources/views/pages/dashboard/navbar.blade.php
+++ src/resources/views/pages/dashboard/navbar.blade.php
@@ -43,11 +43,7 @@
           <div class="relative flex justify-center items-center w-10/12 mx-auto">
             <input type="text" name="query" placeholder="Search" id="small-input" class="d-dashboard__search-bar">
             <button type="submit" class="d-dashboard__search-btn">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="w-5 h-5">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                  d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-              </svg>
+              @include('svg.search')
             </button>
           </div>
         </form>
@@ -58,7 +54,7 @@
             <img class="object-cover w-full h-full" src='{{ auth()->user()->profile->getImageURL() }}'
               alt='Profile Picture'>
           </div>
-          <a href="{{ route('user.profile', ['id' => auth()->id()]) }}">Hello,
+          <a href="{{ route('user.profile', ['id' => auth()->id()]) }}">
             {{ auth()->user()->username }}!</a>
         </div>
       </div>
--- src/resources/views/pages/dashboard/search-result-page.blade.php
+++ src/resources/views/pages/dashboard/search-result-page.blade.php
@@ -1,14 +1,27 @@
 @section('content')
+
 <body>
-    Search results for: {{ request('query') }}
     <div class="d-dashboard__create-post">
+        <p class="mb-2 font-semibold">Search results for: {{ request('query') }}</p>
         @if($results->isNotEmpty())
             @foreach($results as $result)
-                <div><a href="{{ route('user.profile', ['id' => $result->id]) }}">{{ $result->username }}</a></div>
+                <a href="{{ route('user.profile', ['id' => $result->id]) }}">
+                    <div class="flex gap-2 items-center">
+                        <img class="object-cover w-14 h-14 border-2 border-white rounded-full"
+                            src='{{ $result->profile->getImageURL() }}' alt='Profile Picture'>
+                        <div class="flex flex-col">
+                            <div>
+                                {{ $result->username }}
+                            </div>
+                            <div class="font-light text-sm">
+                                {{ $result->profile->address }}
+                            </div>
+                        </div>
+                    </div>
+                </a>
             @endforeach
         @else
             <div>No "{{ request('query') }}" found.</div>
         @endif
     </div>
 </body>
-
