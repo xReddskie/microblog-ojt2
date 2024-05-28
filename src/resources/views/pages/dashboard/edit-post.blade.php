e5d1aa01ea6bd1afaeeee5dac192f55746046597
Hakdoff <erikafuerte09@gmail.com>
2024-05-28 10:54:01 +0800

    Edit post UI

--- src/resources/views/pages/dashboard/edit-post.blade.php
+++ src/resources/views/pages/dashboard/edit-post.blade.php
@@ -1,12 +1,14 @@
 @section('content')
 
-<body>
-<div class="form-popup" id="myForm"> 
-      <form action="/edit-post/{{$post->id}}" method="POST">
-        @csrf
-        @method('PUT')
-        <input type="text" name="content" value="{{$post->content}}">
-          <button><p class="edit">Post</p></button>
-      </form>
-      </div>
-</body>
+    <body>
+        <div class="form-popup" id="myForm">
+            <form action="/edit-post/{{ $post->id }}" method="POST">
+                @csrf
+                @method('PUT')
+                <input type="text" name="content" value="{{ $post->content }}">
+                <button>
+                    <p class="edit">Post</p>
+                </button>
+            </form>
+        </div>
+    </body>
