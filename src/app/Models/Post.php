5ce468bd4c3a45e0999e0eae0af968566f109c6d
PeterMadrid <petermadrid0421@gmail.com>
2024-06-11 10:20:48 +0800

    return type

--- src/app/Models/Post.php
+++ src/app/Models/Post.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -42,7 +43,7 @@ class Post extends Model
     /**
      * Gets the user who liked the post
      */
-    public function likedBy()
+    public function likedBy(): BelongsToMany
     {
         return $this->belongsToMany(User::class, 'likes');
     }
