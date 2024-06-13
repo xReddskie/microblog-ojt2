af542888b96fbca7e45cd5edb633a98f4b5ec4f8
PeterMadrid <petermadrid0421@gmail.com>
2024-06-13 10:47:07 +0800

    converted if statement to switch case

--- src/resources/views/pages/profile/content.blade.php
+++ src/resources/views/pages/profile/content.blade.php
@@ -1,18 +1,28 @@
-@if (!isset($x) || $x == 1)
-    @if (auth()->user()->id == $user->id)
-        @include('pages.dashboard.post')
-    @endif
-    @if (auth()->user()->followees->contains($user->id) || auth()->user()->id == $user->id)
-        @include('pages.dashboard.post-content')
-    @else  
-        @include('pages.dashboard.post-hidden')
-    @endif
-@elseif ($x == 2)
-    @include('pages.profile.nav.follower-lists')
-@elseif ($x == 3)
-    @include('pages.profile.nav.following-lists')
-@elseif ($x == 4)
-    @include('pages.profile.nav.photos')
-@elseif ($x == 5)
-    @include('pages.profile.nav.about')
-@endif
+@switch(true)
+    @case(!isset($x) || $x == 1)
+        @if (auth()->user()->id == $user->id)
+            @include('pages.dashboard.post')
+        @endif
+        @if (auth()->user()->followees->contains($user->id) || auth()->user()->id == $user->id)
+            @include('pages.dashboard.post-content')
+        @else  
+            @include('pages.dashboard.post-hidden')
+        @endif
+        @break
+
+    @case($x == 2)
+        @include('pages.profile.nav.follower-lists')
+        @break
+
+    @case($x == 3)
+        @include('pages.profile.nav.following-lists')
+        @break
+
+    @case($x == 4)
+        @include('pages.profile.nav.photos')
+        @break
+
+    @case($x == 5)
+        @include('pages.profile.nav.about')
+        @break
+@endswitch
